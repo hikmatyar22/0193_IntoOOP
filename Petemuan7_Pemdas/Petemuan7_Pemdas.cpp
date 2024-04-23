dbda7bb72b305a9c229c04617425c15d52a81d70
hikmatyar22 <abiq5161@gmail.com>
2024-04-23 09:52:22 +0700

    membuat beberapa inputan

--- Petemuan7_Pemdas/Petemuan7_Pemdas.cpp
+++ Petemuan7_Pemdas/Petemuan7_Pemdas.cpp
@@ -21,7 +21,12 @@ class matakuliah {
 		int sks;
 	public:
 		void input() {
-
+			cout << "Kode MK : ";
+			cin >> kodeMk;
+			cout << "Nama MK : ";
+			cin >> namaMk;
+			cout << "SKS : ";
+			cin >> sks;
 		}
 
 };
