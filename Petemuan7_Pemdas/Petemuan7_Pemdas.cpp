b5bf0042ad5857582ec2e9fb7b8a2042b898b446
hikmatyar22 <abiq5161@gmail.com>
2024-04-23 09:53:55 +0700

    membuat output pada class matakuliah

--- Petemuan7_Pemdas/Petemuan7_Pemdas.cpp
+++ Petemuan7_Pemdas/Petemuan7_Pemdas.cpp
@@ -15,18 +15,24 @@ public: // akses modifier
 };
 
 class matakuliah {
-	private:
-		string kodeMk;
-		string namaMk;
-		int sks;
-	public:
-		void input() {
-			cout << "Kode MK : ";
-			cin >> kodeMk;
-			cout << "Nama MK : ";
-			cin >> namaMk;
-			cout << "SKS : ";
-			cin >> sks;
-		}
+private:
+	string kodeMk;
+	string namaMk;
+	int sks;
+public:
+	void input() {
+		cout << "Kode MK : ";
+		cin >> kodeMk;
+		cout << "Nama MK : ";
+		cin >> namaMk;
+		cout << "SKS : ";
+		cin >> sks;
+	}
+
+	void output() {
+		cout << "Kode MK : " << kodeMk << endl;
+		cout << "Nama MK : " << namaMk << endl;
+		cout << "SKS : " << sks << endl;
+	}
 
 };
